6c8f1873b0623c92820fae6d701570621b008cbb
NTN1789 <natanalmeida040@gmail.com>
2022-11-10 18:14:42 -0300

    algo

---
+++ meu-projeto/src/App.js
@@ -0,0 +1,16 @@
+import 'App.css'
+
+function App() {
+
+
+ const name = 'Nathan'
+
+  return (
+    <div className="App">
+      <h2>alterando o JSX!</h2>
+      <p> olá ,{name}</p>
+    </div>
+  );
+}
+
+export default App;
